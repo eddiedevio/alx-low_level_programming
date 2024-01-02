f496b9d841aecf01b1085dd453ca0d40a5470d0d
Edward Olujobi <edwardolujobi@yahoo.com>
2024-01-02 11:39:23 -0800

    function that updates a pointer it points to

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -8,27 +8,8 @@
  */
 
 void reset_to_98(int *n)
+
 {
-	printf("Value of 'n' is: %p\n", n)
-	printf("Address of 'n' is: %p\n", n)
-	*n = 98;	
-}
 
-
-int main(void)
-{
-	int n;
-	int *p;
-
-	n = 402;
-       	p = &n;
-
-	printf("The value of 'n' is: %d\n", n);
-	printf("The address of 'n' is: %p\n", &n);
-	printf("The value of 'p' is: %p\n", p);
-	
-	reset_to_98(p);
-	printf("The new value of 'n' is: %d\n", n);
-
-	return (0);
+	*n =98;
 }	
